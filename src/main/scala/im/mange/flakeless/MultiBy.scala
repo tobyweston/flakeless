b1ec55577ee8a91fda9ef04bd1a1376e803b5819
pall <paulallton@gmail.com>
2016-11-24 15:06:47 +0000

    pull up

---
+++ src/main/scala/im/mange/flakeless/MultiBy.scala
@@ -0,0 +1,32 @@
+package im.mange.flakeless
+
+import java.util
+
+import org.openqa.selenium.{By, SearchContext, WebElement}
+
+case class MultiBy(bys: List[By]) extends By {
+  import scala.collection.JavaConverters._
+
+  override def findElements(context: SearchContext): util.List[WebElement] = {
+    val r = bys match {
+      case Nil => throw new RuntimeException("bys must not be empty"); Nil
+      case headBy :: remainingBys => findNext(context.findElements(headBy).asScala.toList, remainingBys)
+    }
+
+    r.asJava
+
+//    WaitForElement()
+//    context.findElements(bys.head)
+//    List.empty[WebElement].asJavaCollection
+  }
+
+  private def findNext(ins: List[WebElement], bys: List[By]): List[WebElement] = {
+    bys match {
+      case Nil => ins
+      case headBy :: remainingBys => {
+        if (ins.length != 1) throw new RuntimeException("by found multiple")
+        else findNext(ins.head.findElements(headBy).asScala.toList, remainingBys)
+      }
+    }
+  }
+}
