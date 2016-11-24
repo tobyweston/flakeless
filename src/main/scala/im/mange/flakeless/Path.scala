29237a10a4b9167d2cb0337a3b9b4270098218d7
pall <paulallton@gmail.com>
2016-11-24 15:43:37 +0000

    be able to add to a path

--- src/main/scala/im/mange/flakeless/Path.scala
+++ src/main/scala/im/mange/flakeless/Path.scala
@@ -4,8 +4,21 @@ import java.util
 
 import org.openqa.selenium.{By, SearchContext, WebElement}
 
-case class Path(bys: By*) extends By {
+object Foo extends App {
+  private val p1 = Path(By.id("parent"))
+  private val p2 = p1.add(By.id("child"))
+
+  println(p1)
+  println(p2)
+  Click(null, p2)
+}
+
+case class Path(private val bys: By*) extends By {
   import scala.collection.JavaConverters._
+
+  def add(by: By) = {
+    Path(by :: bys.toList:_*)
+  }
 
   override def findElements(context: SearchContext): util.List[WebElement] = {
     val r = bys match {
