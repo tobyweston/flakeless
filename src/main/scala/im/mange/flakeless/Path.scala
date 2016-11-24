b4a2300a25d54f615cd3ce5e3d4d81634c947de5
pall <paulallton@gmail.com>
2016-11-24 15:13:31 +0000

    improve message a bit

--- src/main/scala/im/mange/flakeless/Path.scala
+++ src/main/scala/im/mange/flakeless/Path.scala
@@ -4,12 +4,12 @@ import java.util
 
 import org.openqa.selenium.{By, SearchContext, WebElement}
 
-case class MultiBy(bys: By*) extends By {
+case class Path(bys: By*) extends By {
   import scala.collection.JavaConverters._
 
   override def findElements(context: SearchContext): util.List[WebElement] = {
     val r = bys match {
-      case Nil => throw new RuntimeException("bys must not be empty")
+      case Nil => throw new RuntimeException("Path must contain at least one by")
       case headBy :: remainingBys => findNext(context.findElements(headBy).asScala.toList, remainingBys)
     }
 
@@ -30,3 +30,4 @@ case class MultiBy(bys: By*) extends By {
     }
   }
 }
+
