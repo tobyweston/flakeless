7dae7ab1b511886a5057a3a4bc914f02cace3a76
pall <paulallton@gmail.com>
2016-11-24 16:44:31 +0000

    fix some scala gubbins

--- src/main/scala/im/mange/flakeless/Path.scala
+++ src/main/scala/im/mange/flakeless/Path.scala
@@ -13,7 +13,7 @@ case class Path(private val bys: By*) extends By {
 
   override def findElements(context: SearchContext): util.List[WebElement] = {
     val r = bys match {
-      case Nil => throw new RuntimeException("Path must contain at least one By")
+      case all if all.isEmpty => throw new RuntimeException("Path must contain at least one By")
       case headBy :: remainingBys => findNext(context.findElements(headBy).asScala.toList, remainingBys, headBy)
     }
 
