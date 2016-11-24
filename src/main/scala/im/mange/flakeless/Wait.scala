087e9925e040da8980879f5b6067fa8f71fdf6de
pall <paulallton@gmail.com>
2016-11-24 18:09:27 +0000

    seriously sonatype - sort your sh*t out

--- src/main/scala/im/mange/flakeless/Wait.scala
+++ src/main/scala/im/mange/flakeless/Wait.scala
@@ -7,7 +7,7 @@ import scala.annotation.tailrec
 
 object Body {
   //TODO: shouldn't this be polling too?!
-  //TODO: in-fact it should probably be a Path!
+  //TODO: in-fact it should probably be a Path!!
   def apply(webDriver: WebDriver) = webDriver.findElement(By.tagName("body"))
 }
 
