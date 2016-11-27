8b4c90eebc878bbe28ebc6f8decc5523b34015aa
alltonp <paulallton@gmail.com>
2016-11-27 15:35:21 +0000

    converting more commands to use Description

--- src/main/scala/im/mange/flakeless/AssertElementAttributeEquals.scala
+++ src/main/scala/im/mange/flakeless/AssertElementAttributeEquals.scala
@@ -1,6 +1,6 @@
 package im.mange.flakeless
 
-import im.mange.flakeless.innards.{Body, WaitForElement}
+import im.mange.flakeless.innards.{Body, Description, WaitForElement}
 import org.openqa.selenium.{By, WebDriver, WebElement}
 
 object AssertElementAttributeEquals {
@@ -10,7 +10,11 @@ object AssertElementAttributeEquals {
 
   def apply(in: WebElement, by: By, attribute: String, expected: String): Unit = {
     WaitForElement(in, by,
-      description = e => s"AssertElementAttributeEquals\n| in: $in\n| $by\n| attribute: '$attribute'\n| expected: '$expected'\n| but was: '${e.getAttribute(attribute)}'",
+      description = e =>
+        Description("AssertElementAttributeEquals", in, by, args = Map("attribute" -> attribute), expected = Some(expected),
+          actual = Some((e) => e.getAttribute(attribute)))
+          .describe(e),
+
       condition = e => e.getAttribute(attribute) == expected)
   }
 }
--- src/main/scala/im/mange/flakeless/AssertElementCountEquals.scala
+++ src/main/scala/im/mange/flakeless/AssertElementCountEquals.scala
@@ -1,6 +1,6 @@
 package im.mange.flakeless
 
-import im.mange.flakeless.innards.{Body, WaitForElements}
+import im.mange.flakeless.innards.{Body, Description, WaitForElements}
 import org.openqa.selenium.{By, WebDriver, WebElement}
 
 object AssertElementCountEquals {
@@ -8,9 +8,10 @@ object AssertElementCountEquals {
     apply(Body(webDriver), by, expected)
   }
 
+  //TODO: I need to be converted to a Description
   def apply(in: WebElement, by: By, expected: Int): Unit = {
     WaitForElements(in, by,
-      description = e => s"AssertElementCountEquals\n| in: $in\n| $by\n| expected: '$expected'\n| but was: '${e.size}'",
-      condition = e => e.size == expected)
+      description = es => s"AssertElementCountEquals\n| in: $in\n| $by\n| expected: '$expected'\n| but was: '${es.size}'",
+      condition = es => es.size == expected)
   }
 }
