c7aefa8b12cf097d352d3a0d15fcd0ab6c4eaded
pall <paulallton@gmail.com>
2016-11-29 08:47:58 +0000

    trigger build because sonatype is flakey ...

--- src/main/scala/im/mange/flakeless/AssertElementListTextContains.scala
+++ src/main/scala/im/mange/flakeless/AssertElementListTextContains.scala
@@ -8,11 +8,11 @@ object AssertElementListTextContains {
     apply(Body(webDriver), by, expected)
   }
 
-  //TODO: I need to be converted to a Description, just not possible yet
+  //TODO: I need to be converted to a Description, just not possible yet.
   def apply(in: WebElement, by: By, expected: String): Unit = {
     WaitForElements(in, by,
 
-      description = es => s"AssertElementListTextContains\n| in: $in\n| $by\n| expected: '$expected'\n| but was: '${es.map(t => s"'${t.getText}'").mkString(", ")}'",
+       description = es => s"AssertElementListTextContains\n| in: $in\n| $by\n| expected: '$expected'\n| but was: '${es.map(t => s"'${t.getText}'").mkString(", ")}'",
 
       condition = es => es.map(_.getText).contains(expected))
   }
