88feba7825c2bdae8a17f43b508cab1cff8dcdee
alltonp <paulallton@gmail.com>
2016-12-05 22:00:29 +0000

    first stab at at a phantom file uploader

---
+++ src/main/scala/im/mange/flakeless/UploadFilePhantomJs.scala
@@ -0,0 +1,22 @@
+package im.mange.flakeless
+
+import im.mange.flakeless.innards.{Body, Description, WaitForInteractableElement}
+import org.openqa.selenium.phantomjs.PhantomJSDriver
+import org.openqa.selenium.{By, WebDriver, WebElement}
+
+object UploadFilePhantomJs {
+  def apply(in: WebDriver, by: By, filename: String): Unit = {
+    val webElement = Body(in)
+
+    WaitForInteractableElement(webElement, by,
+
+      description = e => Description("UploadFilePhantomJs", webElement, by, args = Map("filename" -> filename)).describe(e),
+
+      action = e => {
+        in.asInstanceOf[PhantomJSDriver].executePhantomJS("var page = this; page.uploadFile('" + by + "', '" + filename + "');")
+      },
+
+      mustBeDisplayed = false
+    )
+  }
+}
