17a2ebbc9efac178f4b34708e38310c01b42c80d
alltonp <paulallton@gmail.com>
2016-12-05 21:03:23 +0000

    add AssertElementClassEquals

---
+++ src/main/scala/im/mange/flakeless/AssertElementClassEquals.scala
@@ -0,0 +1,23 @@
+package im.mange.flakeless
+
+import im.mange.flakeless.innards.{Body, Description, WaitForElement}
+import org.openqa.selenium.{By, WebDriver, WebElement}
+
+object AssertElementClassEquals {
+  def apply(webDriver: WebDriver, by: By, expected: String): Unit = {
+    apply(Body(webDriver), by, expected)
+  }
+
+  def apply(in: WebElement, by: By, expected: String): Unit = {
+    val attribute = "class"
+
+    WaitForElement(in, by,
+
+      description = e =>
+        Description("AssertElementClassEquals", in, by, args = Map("attribute" -> attribute), expected = Some(expected),
+          actual = Some((e) => e.getAttribute(attribute)))
+          .describe(e),
+
+      condition = e => e.getAttribute(attribute) == expected)
+  }
+}
