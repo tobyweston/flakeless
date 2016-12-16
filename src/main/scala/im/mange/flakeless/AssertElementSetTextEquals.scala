bc9482fc8d9520a1b6359f3b1a239a846d4e8827
pall <paulallton@gmail.com>
2016-12-16 10:48:02 -0500

    add AssertElementSetTextEquals

---
+++ src/main/scala/im/mange/flakeless/AssertElementSetTextEquals.scala
@@ -0,0 +1,19 @@
+package im.mange.flakeless
+
+import im.mange.flakeless.innards.{Body, WaitForElements}
+import org.openqa.selenium.{By, WebDriver, WebElement}
+
+object AssertElementSetTextEquals {
+  def apply(webDriver: WebDriver, by: By, expected: Set[String]): Unit = {
+    apply(Body(webDriver), by, expected)
+  }
+
+  //TODO: I need to be converted to a Description, just not possible yet..
+  def apply(in: WebElement, by: By, expected: Set[String]): Unit = {
+    WaitForElements(in, by,
+
+      description = es => s"AssertElementSetTextEquals\n| in: $in\n| $by\n| expected: '$expected'\n| but was: '${es.map(t => s"'${t.getText}'").mkString(", ")}'",
+
+      condition = es => es.map(_.getText).toSet == expected)
+  }
+}
