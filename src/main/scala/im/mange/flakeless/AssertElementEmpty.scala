cad34658c9166e4c53b6f997290efcaa9244b4e1
pall <paulallton@gmail.com>
2017-04-04 08:48:34 +0100

    add AssertElementEmpty

---
+++ src/main/scala/im/mange/flakeless/AssertElementEmpty.scala
@@ -0,0 +1,23 @@
+package im.mange.flakeless
+
+import im.mange.flakeless.innards.{Body, Description, WaitForElement}
+import org.openqa.selenium.{By, WebDriver, WebElement}
+
+object AssertElementEmpty {
+  def apply(webDriver: WebDriver, by: By): Unit = {
+    apply(Body(webDriver), by)
+  }
+
+  def apply(in: WebElement, by: By): Unit = {
+    WaitForElement(in, by,
+
+      description = e =>
+        Description("AssertElementEmpty", in, by, expected = Some(""),
+          actual = Some((e) =>
+            e.getText ++ " and " ++ e.findElements(By.xpath(".//*")).size.toString ++ " children "
+          )
+        ).describe(e),
+
+      condition = e => e.findElements(By.xpath(".//*")).size == 0 && e.getText.isEmpty)
+  }
+}
