46143fd6ddf20d8cb7eb675918ad8998048eb0dd
pall <paulallton@gmail.com>
2017-05-18 07:50:51 +0100

    add rudimentary FDR

--- src/main/scala/im/mange/flakeless/Flakeless.scala
+++ src/main/scala/im/mange/flakeless/Flakeless.scala
@@ -3,5 +3,39 @@ package im.mange.flakeless
 import org.openqa.selenium.WebDriver
 
 case class Flakeless(driver: WebDriver) {
+  private val flight = new AtomicIntCounter(0)
+  private val fdr = FlightDataRecorder()
 
+  def nextFlight(description: Option[String] = None) {
+    flight.next
+  }
+
+  def record(data: String) {
+    fdr.record(flight.value, data)
+  }
+}
+
+case class DataPoint(who: Int, when: Long, what: Any)
+
+case class FlightDataRecorder() {
+  private val dataByFlight: scala.collection.concurrent.TrieMap[Int, Seq[DataPoint]] =
+    new scala.collection.concurrent.TrieMap()
+
+  def record(flight: Int, data: String) {
+    val current = dataByFlight.getOrElse(flight, Seq.empty[DataPoint])
+    dataByFlight.update(flight, current :+ DataPoint(flight, System.currentTimeMillis(), data))
+  }
+
+  def data(flight: Int) = dataByFlight(flight)
+}
+
+class AtomicIntCounter(start: Int = 1) {
+  private var count = start - 1
+
+  def next = synchronized {
+    count += 1
+    count
+  }
+
+  def value = count
 }
