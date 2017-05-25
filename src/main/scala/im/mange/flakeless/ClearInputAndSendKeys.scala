e3cbdb0f4e7a309c1a8fa66505e709f1df4a6058
pall <paulallton@gmail.com>
2017-05-25 09:15:54 +0100

    restore the char sequence ness of typing

--- src/main/scala/im/mange/flakeless/ClearInputAndSendKeys.scala
+++ src/main/scala/im/mange/flakeless/ClearInputAndSendKeys.scala
@@ -8,7 +8,17 @@ object ClearInputAndSendKeys {
     apply(Body(flakeless.rawWebDriver), by, keysToSend, Some(flakeless))
   }
 
+  def apply(flakeless: Flakeless, by: By, keysToSend: CharSequence*): Unit = {
+    apply(Body(flakeless.rawWebDriver), by, keysToSend.toString(), Some(flakeless))
+  }
+
   def apply(in: WebElement, by: By, keysToSend: String, flakeless: Option[Flakeless] = None): Unit = {
+    new ClearInputAndSendKeys(flakeless, in, by, keysToSend).execute()
+  }
+}
+
+private class ClearInputAndSendKeys(flakeless: Option[Flakeless], in: WebElement, by: By, keysToSend: String) {
+  def execute(): Unit = {
     WaitForInteractableElement(flakeless, in, by,
 
       description = e => Description("ClearInputAndSendKeys", in, by, args = Map("keysToSend" -> keysToSend.toString())).describe(e),
--- src/main/scala/im/mange/flakeless/SendKeys.scala
+++ src/main/scala/im/mange/flakeless/SendKeys.scala
@@ -3,15 +3,26 @@ package im.mange.flakeless
 import im.mange.flakeless.innards.{Body, Description, WaitForInteractableElement}
 import org.openqa.selenium.{By, WebDriver, WebElement}
 
+//TODO: should this be Enter? it isnt in webdriver
 object SendKeys {
   def apply(flakeless: Flakeless, by: By, keysToSend: String): Unit = {
     apply(Body(flakeless.rawWebDriver), by, keysToSend, Some(flakeless))
   }
 
+  def apply(flakeless: Flakeless, by: By, keysToSend: CharSequence*): Unit = {
+    apply(Body(flakeless.rawWebDriver), by, keysToSend.toString(), Some(flakeless))
+  }
+
   def apply(in: WebElement, by: By, keysToSend: String, flakeless: Option[Flakeless] = None): Unit = {
+    new SendKeys(flakeless, in, by, keysToSend).execute()
+  }
+}
+
+private class SendKeys(flakeless: Option[Flakeless], in: WebElement, by: By, keysToSend: String) {
+  def execute(): Unit = {
     WaitForInteractableElement(flakeless, in, by,
 
-      description = e => Description("SendKeys", in, by, args = Map("keysToSend" -> keysToSend.toString())).describe(e),
+      description = e => Description("SendKeys", in, by, args = Map("keysToSend" -> keysToSend)).describe(e),
 
       action = e => e.sendKeys(keysToSend)
     )
