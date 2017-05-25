4147c4d0b0ffbb2ccb819d515cbbc79ee5f5dd97
pall <paulallton@gmail.com>
2017-05-25 13:10:38 +0100

    a little more private

--- src/main/scala/im/mange/flakeless/innards/AtomicIntCounter.scala
+++ src/main/scala/im/mange/flakeless/innards/AtomicIntCounter.scala
@@ -1,9 +1,6 @@
 package im.mange.flakeless.innards
 
-/**
-  * Created by pall on 25/05/2017.
-  */
-case class AtomicIntCounter(start: Int = 1) {
+private [flakeless] case class AtomicIntCounter(start: Int = 1) {
   private var count = start - 1
 
   def next = synchronized {
