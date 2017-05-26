f01c44c9ec4f0b2f3d276c0ec1d6f8aecdcd4b9d
pall <paulallton@gmail.com>
2017-05-26 10:34:10 +0100

    put an end to var args

--- src/main/scala/im/mange/flakeless/ClearInputAndSendKeys.scala
+++ src/main/scala/im/mange/flakeless/ClearInputAndSendKeys.scala
@@ -3,29 +3,27 @@ package im.mange.flakeless
 import im.mange.flakeless.innards.{Body, Description, WaitForInteractableElement}
 import org.openqa.selenium.{By, WebDriver, WebElement}
 
+//TODO: should this be Enter? it isnt in webdriver
+//TODO: this should share with SendKeys
 object ClearInputAndSendKeys {
-  def apply(flakeless: Flakeless, by: By, keysToSend: String): Unit = {
+  def apply(flakeless: Flakeless, by: By, keysToSend: List[CharSequence]): Unit = {
     apply(Body(flakeless.rawWebDriver), by, keysToSend, Some(flakeless))
   }
 
-  def apply(flakeless: Flakeless, by: By, keysToSend: CharSequence*): Unit = {
-    apply(Body(flakeless.rawWebDriver), by, keysToSend.toList.toString(), Some(flakeless))
-  }
-
-  def apply(in: WebElement, by: By, keysToSend: String, flakeless: Option[Flakeless] = None): Unit = {
+  def apply(in: WebElement, by: By, keysToSend: List[CharSequence], flakeless: Option[Flakeless] = None): Unit = {
     new ClearInputAndSendKeys(flakeless, in, by, keysToSend).execute()
   }
 }
 
-private class ClearInputAndSendKeys(flakeless: Option[Flakeless], in: WebElement, by: By, keysToSend: String) {
+private class ClearInputAndSendKeys(flakeless: Option[Flakeless], in: WebElement, by: By, keysToSend: List[CharSequence]) {
   def execute(): Unit = {
     WaitForInteractableElement(flakeless, in, by,
 
-      description = e => Description("ClearInputAndSendKeys", in, by, args = Map("keysToSend" -> keysToSend.toString())).describe(e),
+      description = e => Description("ClearInputAndSendKeys", in, by, args = Map("keysToSend" -> keysToSend.mkString)).describe(e),
 
       action = e => {
         e.clear()
-        e.sendKeys(keysToSend)
+        e.sendKeys(keysToSend:_*)
       }
 
     )
--- src/main/scala/im/mange/flakeless/SendKeys.scala
+++ src/main/scala/im/mange/flakeless/SendKeys.scala
@@ -4,27 +4,24 @@ import im.mange.flakeless.innards.{Body, Description, WaitForInteractableElement
 import org.openqa.selenium.{By, WebDriver, WebElement}
 
 //TODO: should this be Enter? it isnt in webdriver
+//TODO: this should share with ClearAndSendKeys
 object SendKeys {
-  def apply(flakeless: Flakeless, by: By, keysToSend: String): Unit = {
+  def apply(flakeless: Flakeless, by: By, keysToSend: List[CharSequence]): Unit = {
     apply(Body(flakeless.rawWebDriver), by, keysToSend, Some(flakeless))
   }
 
-  def apply(flakeless: Flakeless, by: By, keysToSend: CharSequence*): Unit = {
-    apply(Body(flakeless.rawWebDriver), by, keysToSend.toList.toString(), Some(flakeless))
-  }
-
-  def apply(in: WebElement, by: By, keysToSend: String, flakeless: Option[Flakeless] = None): Unit = {
+  def apply(in: WebElement, by: By, keysToSend: List[CharSequence], flakeless: Option[Flakeless] = None): Unit = {
     new SendKeys(flakeless, in, by, keysToSend).execute()
   }
 }
 
-private class SendKeys(flakeless: Option[Flakeless], in: WebElement, by: By, keysToSend: String) {
+private class SendKeys(flakeless: Option[Flakeless], in: WebElement, by: By, keysToSend: List[CharSequence]) {
   def execute(): Unit = {
     WaitForInteractableElement(flakeless, in, by,
 
-      description = e => Description("SendKeys", in, by, args = Map("keysToSend" -> keysToSend)).describe(e),
+      description = e => Description("SendKeys", in, by, args = Map("keysToSend" -> keysToSend.mkString)).describe(e),
 
-      action = e => e.sendKeys(keysToSend)
+      action = e => e.sendKeys(keysToSend:_*)
     )
   }
 }
