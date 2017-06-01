4ae165405ecb5a6e8cc9a80623ee141f8b445018
pall <paulallton@gmail.com>
2017-06-01 08:30:14 +0100

    delay execution for WFIE

--- src/main/scala/im/mange/flakeless/innards/WaitForInteractableElement.scala
+++ src/main/scala/im/mange/flakeless/innards/WaitForInteractableElement.scala
@@ -8,8 +8,20 @@ object WaitForInteractableElement {
             description: (WebElement) => String,
             condition: (WebElement) => Boolean = (e) => {true},
             action: (WebElement) => Unit,
-            mustBeDisplayed: Boolean = true) = {
+            mustBeDisplayed: Boolean = true) {
 
+    new WaitForInteractableElement(in, by, description, condition, action, mustBeDisplayed)
+      .execute(flakeless)
+  }
+}
+
+class WaitForInteractableElement(in: WebElement, by: By,
+            description: (WebElement) => String,
+            condition: (WebElement) => Boolean = (e) => {true},
+            action: (WebElement) => Unit,
+            mustBeDisplayed: Boolean = true) {
+
+  def execute(flakeless: Option[Flakeless]) {
     //TODO: we should ensure there is only one element - make configurable
     Wait.waitUpTo().forCondition(
       {
