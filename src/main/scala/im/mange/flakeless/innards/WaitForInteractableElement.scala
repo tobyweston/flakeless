5f7466bb61c3921314f15709d1d1b215ed91b2cb
pall <paulallton@gmail.com>
2017-06-01 08:53:51 +0100

    movin things round a bit

--- src/main/scala/im/mange/flakeless/innards/WaitForInteractableElement.scala
+++ src/main/scala/im/mange/flakeless/innards/WaitForInteractableElement.scala
@@ -10,16 +10,20 @@ object WaitForInteractableElement {
             action: (WebElement) => Unit,
             mustBeDisplayed: Boolean = true) {
 
-    new WaitForInteractableElement(Context(), in, by, description, condition, action, mustBeDisplayed)
-      .execute(flakeless)
+    Execute(flakeless,
+      new WaitForInteractableElement(Context(), in, by, description, condition, action, mustBeDisplayed))
   }
+}
+
+trait Executable {
+  def execute(flakeless: Option[Flakeless])
 }
 
 private class WaitForInteractableElement(context: Context, in: WebElement, by: By,
             description: (WebElement) => String,
             condition: (WebElement) => Boolean = (e) => {true},
             action: (WebElement) => Unit,
-            mustBeDisplayed: Boolean = true) {
+            mustBeDisplayed: Boolean = true) extends Executable {
 
   def execute(flakeless: Option[Flakeless]) {
     //TODO: we should ensure there is only one element - make configurable
@@ -28,7 +32,7 @@ private class WaitForInteractableElement(context: Context, in: WebElement, by: B
         val e = in.findElement(by)
         val result = (if (mustBeDisplayed) e.isDisplayed else true) && e.isEnabled && condition(e)
         val value = description(in.findElement(by))
-
+        //TODO: ultimately don't do this
         flakeless.foreach(_.record(result, value))
         context.remember(result, value)
         result
@@ -36,5 +40,12 @@ private class WaitForInteractableElement(context: Context, in: WebElement, by: B
       description(in.findElement(by)),
       action(in.findElement(by))
     )
+  }
+}
+
+object Execute {
+  def apply(flakeless: Option[Flakeless], executable: Executable): Unit = {
+    //TODO: probably should pass the context in here instead
+    executable.execute(flakeless)
   }
 }
