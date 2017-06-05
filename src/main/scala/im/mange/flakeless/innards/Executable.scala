21e4ea652d0a14825c3f36aca37adb716c96aa02
pall <paulallton@gmail.com>
2017-06-05 16:33:44 +0100

    a little tidying

--- src/main/scala/im/mange/flakeless/innards/Executable.scala
+++ src/main/scala/im/mange/flakeless/innards/Executable.scala
@@ -10,13 +10,13 @@ trait Executable {
 //TODO: probably rename to Command
 //TODO: improve rendering of options and in etc ...
 //TODO: args at end? expected's earlier?
-case class Intention(command: String, in: WebElement, by: By,
+case class Intention(name: String, in: WebElement, by: By,
                      args: Map[String, String] = Map.empty,
                      expected: Option[String] = None,
                      expectedMany: Option[List[String]] = None) {
 
   //TODO: this is all well hokey
-  def describe = reallyDescribe(None)
+  def describe = reallyDescribe
 
   //TODO: ultimately shouldn't need this here, extract formatter
   case class LabelAndValue(label: Option[String], value: String) {
@@ -27,14 +27,12 @@ case class Intention(command: String, in: WebElement, by: By,
   }
 
   //TODO: render expectedMany
-  private def reallyDescribe(webElement: Option[WebElement]): String = {
+  private def reallyDescribe: String = {
     (
       Seq(
-        Some(LabelAndValue(None, command)),
+        Some(LabelAndValue(None, name)),
         Some(LabelAndValue(Some("in"), in.toString)),
-        Some(LabelAndValue(Some("by"), by.toString)),
-        webElement.map(e => LabelAndValue(Some("displayed"), e.isDisplayed.toString)),
-        webElement.map(e => LabelAndValue(Some("enabled"), e.isEnabled.toString))
+        Some(LabelAndValue(Some("by"), by.toString))
       ) ++
         args.map(kv => Some(LabelAndValue(Some(kv._1), kv._2))) ++
         Seq(
