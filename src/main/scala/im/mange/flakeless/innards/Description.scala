a04425673362b41924f572e86c598db60085fb21
pall <paulallton@gmail.com>
2017-06-05 07:58:29 +0100

    start to make it possible to render the description intent

--- src/main/scala/im/mange/flakeless/innards/Description.scala
+++ src/main/scala/im/mange/flakeless/innards/Description.scala
@@ -11,28 +11,37 @@ case class Description(command: String, in: WebElement, by: By, args: Map[String
     }
   }
 
-  def describeActual(webElement: WebElement) = {
+  def describeIntent = {
     try {
-      reallyDescribe(webElement)
+      reallyDescribe(None)
     }
     catch {
       case e: Exception => s"Exception thrown while describing $this cause:\n" + e.getMessage
     }
   }
 
-  private def reallyDescribe(webElement: WebElement): String = {
+  def describeActual(webElement: WebElement) = {
+    try {
+      reallyDescribe(Some(webElement))
+    }
+    catch {
+      case e: Exception => s"Exception thrown while describing $this cause:\n" + e.getMessage
+    }
+  }
+
+  private def reallyDescribe(webElement: Option[WebElement]): String = {
     (
       Seq(
         Some(LabelAndValue(None, command)),
         Some(LabelAndValue(Some("in"), in.toString)),
         Some(LabelAndValue(Some("by"), by.toString)),
-        if (!webElement.isDisplayed) Some(LabelAndValue(Some("displayed"), webElement.isDisplayed.toString)) else None,
-        if (!webElement.isEnabled) Some(LabelAndValue(Some("enabled"), webElement.isEnabled.toString)) else None
+        webElement.map(e => LabelAndValue(Some("displayed"), e.isDisplayed.toString)),
+        webElement.map(e => LabelAndValue(Some("enabled"), e.isEnabled.toString))
       ) ++
         args.map(kv => Some(LabelAndValue(Some(kv._1), kv._2))) ++
         Seq(
-          expected.map(e => LabelAndValue(Some("expected"), e)),
-          actual.map(bw => LabelAndValue(Some("actual"), butWasSafely(webElement, bw)))
+          expected.map(e => LabelAndValue(Some("expected"), e))//,
+//          actual.map(bw => LabelAndValue(Some("actual"), butWasSafely(webElement, bw)))
         )
       ).flatten.map(_.describe).mkString("\n| ")
   }
