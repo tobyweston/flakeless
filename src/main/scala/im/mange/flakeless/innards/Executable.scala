947316f10504f730ccc00f2d65ad1b5c4f2d458d
pall <paulallton@gmail.com>
2017-06-05 10:43:35 +0100

    thinking about broken descriptions a bit

--- src/main/scala/im/mange/flakeless/innards/Executable.scala
+++ src/main/scala/im/mange/flakeless/innards/Executable.scala
@@ -1,5 +1,6 @@
 package im.mange.flakeless.innards
 
+import im.mange.flakeless.innards.Description.LabelAndValue
 import org.openqa.selenium.{By, WebElement}
 
 trait Executable {
@@ -14,5 +15,35 @@ case class Intention(command: String, in: WebElement, by: By,
                      args: Map[String, String] = Map.empty,
                      expected: Option[String] = None,
                      expectedMany: Option[List[String]] = None) {
+
+
   def describe = s"$command $by $in $args $expected $expectedMany"
+
+//  //
+//  def describe = reallyDescribe(None)
+//
+//  case class LabelAndValue(label: Option[String], value: String) {
+//    def describe = label match {
+//      case Some(l) => s"$l: '$value'"
+//      case None => value
+//    }
+//  }
+//
+//  private def reallyDescribe(webElement: Option[WebElement]): String = {
+//    (
+//      Seq(
+//        Some(LabelAndValue(None, command)),
+//        Some(LabelAndValue(Some("in"), in.toString)),
+//        Some(LabelAndValue(Some("by"), by.toString)),
+//        webElement.map(e => LabelAndValue(Some("displayed"), e.isDisplayed.toString)),
+//        webElement.map(e => LabelAndValue(Some("enabled"), e.isEnabled.toString))
+//      ) ++
+//        args.map(kv => Some(LabelAndValue(Some(kv._1), kv._2))) ++
+//        Seq(
+//          expected.map(e => LabelAndValue(Some("expected"), e))
+////          actual.map(bw => LabelAndValue(Some("actual"), butWasSafely(webElement, bw)))
+//        )
+//      ).flatten.map(_.describe).mkString("\n| ")
+//  }
+
 }
