b70b54c88b7ef327b776f3dd38c1bf65f351a9f5
pall <paulallton@gmail.com>
2017-08-23 08:40:42 +0100

    a little tidying

--- src/main/scala/im/mange/flakeless/innards/FlightDataRecorder.scala
+++ src/main/scala/im/mange/flakeless/innards/FlightDataRecorder.scala
@@ -1,14 +1,11 @@
 package im.mange.flakeless.innards
 
-import java.nio.file.Paths
-
-import im.mange.little.file.Filepath
 import im.mange.little.json.{LittleJodaSerialisers, LittleSerialisers}
 import org.joda.time.DateTime
-import org.json4s.{NoTypeHints, _}
-import org.json4s.native.{JsonParser, Serialization}
-import org.json4s.native.Serialization._
+import org.json4s.NoTypeHints
 import org.json4s.native.JsonMethods._
+import org.json4s.native.Serialization._
+import org.json4s.native.{JsonParser, Serialization}
 
 //TODO: pull out the json bit to another thing
 //TODO: might be able to drop the whole flight number thing from the json soon
@@ -35,15 +32,6 @@ private [flakeless] case class FlightDataRecorder() {
   private def append(flightNumber: Int, dataPoint: DataPoint): Unit = {
     val current = dataByFlightNumber.getOrElse(flightNumber, Seq.empty[DataPoint])
     dataByFlightNumber.update(flightNumber, current :+ dataPoint)
-  }
-
-  private def writeReport(flightNumber: Int, dataPoints: Seq[DataPoint], outputDirectory: String) {
-    //TODO: we should delegate to the TestRunRegistry for this ...
-//    val directory = Directory(outputDirectory)
-//    if (!directory.exists) directory.createDirectory(force = true)
-    val filepath = outputDirectory + "/" + flightNumber + ".json"
-    val content = Json.serialise(dataPoints)
-    Filepath.save(content, Paths.get(filepath))
   }
 }
 
