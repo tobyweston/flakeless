b999ff381749f21706598e767304c3942614a9c0
pall <paulallton@gmail.com>
2018-02-05 17:39:23 +0000

    add missing primitives

--- src/main/scala/im/mange/flakeless/FluentDriver.scala
+++ src/main/scala/im/mange/flakeless/FluentDriver.scala
@@ -2,7 +2,6 @@ package im.mange.flakeless
 
 import org.openqa.selenium.By
 
-//TODO: fill me out with all available primitives
 trait FluentDriver {
   val flakeless: Flakeless
 
@@ -11,10 +10,86 @@ trait FluentDriver {
   }
 
   def assertElementAttributeEquals(by: By, attribute: String, expected: String): this.type = {
-    AssertElementAttributeEquals(flakeless: Flakeless, by, attribute, expected); this
+    AssertElementAttributeEquals(flakeless, by, attribute, expected); this
+  }
+
+  def assertElementClassContains(by: By, expected: String): this.type = {
+    AssertElementClassContains(flakeless, by, expected); this
+  }
+
+  def assertElementClassEquals(by: By, expected: String): this.type = {
+    AssertElementClassEquals(flakeless, by, expected); this
+  }
+
+  def assertElementDisabled(by: By): this.type = {
+    AssertElementDisabled(flakeless, by); this
+  }
+
+  def assertElementDisplayed(by: By): this.type = {
+    AssertElementDisplayed(flakeless, by); this
+  }
+
+  def assertElementEmpty(by: By): this.type = {
+    AssertElementEmpty(flakeless, by); this
+  }
+
+  def assertElementEnabled(by: By): this.type = {
+    AssertElementEnabled(flakeless, by); this
+  }
+
+  def assertElementHidden(by: By): this.type = {
+    AssertElementHidden(flakeless, by); this
   }
 
   def assertElementListCountEquals(by: By, expected: Int): this.type = {
-    AssertElementListCountEquals(flakeless: Flakeless, by, expected); this
+    AssertElementListCountEquals(flakeless, by, expected); this
+  }
+
+  def assertElementListTextContains(by: By, expected: String) : this.type = {
+    AssertElementListTextContains(flakeless, by, expected); this
+  }
+
+  def assertElementListTextEquals(by: By, expected: List[String]) : this.type = {
+    AssertElementListTextEquals(flakeless, by, expected); this
+  }
+
+  def assertElementSelected(by: By) : this.type = {
+    AssertElementSelected(flakeless, by); this
+  }
+
+  def assertElementSetTextEquals(by: By, expected: Set[String]) : this.type = {
+    AssertElementSetTextEquals(flakeless, by, expected); this
+  }
+
+  def assertElementTextContains(by: By, expected: String) : this.type = {
+    AssertElementTextContains(flakeless, by, expected); this
+  }
+
+  def assertElementTextEquals(by: By, expected: String) : this.type = {
+    AssertElementTextEquals(flakeless, by, expected); this
+  }
+
+  def assertElementUnselected(by: By) : this.type = {
+    AssertElementUnselected(flakeless, by); this
+  }
+
+  def clearAndSendKeys(by: By, keysToSend: String) : this.type = {
+    ClearInputAndSendKeys(flakeless, by, keysToSend); this
+  }
+
+  def click(by: By) : this.type = {
+    Click(flakeless, by); this
+  }
+
+  def goto(url: String) : this.type = {
+    Goto(flakeless, url); this
+  }
+
+  def sendKeys(by: By, keysToSend: String) : this.type = {
+    SendKeys(flakeless, by, keysToSend); this
+  }
+
+  def uploadFile(by: By, filename: String) : this.type = {
+    UploadFile(flakeless, by, filename); this
   }
 }
