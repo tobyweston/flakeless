b1d36e55643c844c662893e914f5d7d59d9fbfd3
pall <paulallton@gmail.com>
2018-02-21 17:45:30 +0000

    change default report dir

--- src/main/scala/im/mange/flakeless/Config.scala
+++ src/main/scala/im/mange/flakeless/Config.scala
@@ -1,5 +1,5 @@
 package im.mange.flakeless
 
 case class Config(waitPollPeriod: Int = 1, waitTimeout: Int = 5000
-                  , reportDirectory: String = "target/test-reports"
+                  , reportDirectory: String = "target/flakeless"
                   , reportHost: Option[String] = Some("http://localhost:63342/root"))
